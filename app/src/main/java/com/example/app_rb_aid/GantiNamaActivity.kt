e6ae20a5453f81ca2881cbaf9cc37d814897b9b0
MHafidzAlFarisi <muhammadhafidzalfarisi@mail.ugm.ac.id>
2025-09-09 21:21:14 +0700

    Tambah ganti nama dan ganti pass

---
+++ app/src/main/java/com/example/app_rb_aid/GantiNamaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.app_rb_aid
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.app_rb_aid.databinding.ActivityGantiNamaBinding
+
+class GantiNamaActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityGantiNamaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
